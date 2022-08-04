35a0b50acfbdc9a9b25648a03acd2c57c17174ec
Nabor Torres <arsentau@gmail.com>
2022-08-04 17:35:52 -0300

    Add config logic with pydantic

--- src/core/config.py
+++ src/core/config.py
@@ -1,16 +1,43 @@
-from pydantic import BaseModel
-from typing import List
+from functools import lru_cache
+from typing import List, Optional
 
-class Api(BaseModel):
-    version: str
-    title: str
-    debug: bool
+from pydantic import BaseSettings
 
-class AllowedCors(BaseModel):
-    hosts: List[str]
-    methods: List[str]
-    headers: List[str]
 
-class Config(BaseModel):
-    api: Api
-    allowed_cors: AllowedCors
+class ApiSettings(BaseSettings):
+    version: Optional[str] = "Default Mode"
+    debug: Optional[bool] = True
+    host: Optional[str] = "127.0.0.1"
+    port: Optional[int] = 8000
+    allow_origins: Optional[List[str]] = ["*"]
+    allow_credentials: Optional[List[str]] = ["*"]
+    allow_methods: Optional[List[str]] = ["*"]
+    allow_headers: Optional[List[str]] = ["*"]
+    title: Optional[str] = "FastApi Poc"
+
+    class Config:
+        env_file = ".env"
+
+
+class DbSettings(BaseSettings):
+    postgres_host: str
+    postgres_port: int
+    postgres_user: str
+    postgres_password: str
+    postgres_name: str
+    pgadmin_default_email: str
+    pgadmin_default_password: str
+
+    class Config:
+        env_file = ".env.database"
+
+
+class Settings:
+
+    @lru_cache
+    def get_api_settings() -> None:
+        return ApiSettings()
+
+    @lru_cache
+    def get_db_settings() -> None:
+        return DbSettings()
