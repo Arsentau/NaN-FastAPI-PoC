e34f1b8c3a9c76c9ffb672f46f360d4eb8e47309
Nabor Torres <arsentau@gmail.com>
2022-08-22 17:40:41 -0300

    Add basic Company Model and Employee model

---
+++ src/db/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column
+from sqlalchemy.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import Boolean, String
+
+from src.db.database import Base
+
+
+class Company(Base):
+    __tablename__ = 'company'
+    id = Column(String, primary_key=True, index=True)
+    company_name = Column(String)
+
+
+class Employee(Base):
+    __tablename__ = 'employee'
+    id = Column(String, primary_key=True, index=True, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    address = Column(String)
+    city = Column(String)
+    state_province = Column(String)
+    country = Column(String)
+    zip_code = Column(String)
+    time_zone = Column(String)
+    personal_id = Column(String)
+    email = Column(String, unique=True)
+    phone_number = Column(String)
+    is_manager = Column(Boolean)
+    company = Column(String, ForeignKey('company.id'))
+    role = Column(String)
