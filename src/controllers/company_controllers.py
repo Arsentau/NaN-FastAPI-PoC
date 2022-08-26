10e2103767d9484a99140c6b6d02176299c5e1f2
Nabor Torres <arsentau@gmail.com>
2022-08-26 13:04:26 -0300

    Add company controllers

--- src/controllers/company_controllers.py
+++ src/controllers/company_controllers.py
@@ -1,15 +1,30 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+from db.database import get_db
+from models.schemas.company import CompanySchema
+from services.company_service import CompanyService
 
 router = APIRouter()
 
 
 @router.get(
+    "/mock-company",
+    status_code=status.HTTP_201_CREATED,
+    name="Create and get mock company",
+    response_model=CompanySchema
+)
+async def mock_company_creator(db: Session = Depends(get_db)):
+    return CompanyService.bulk_reactor(db)
+
+
+@router.get(
     "/",
     status_code=status.HTTP_200_OK,
-    name="Get All companies",
+    name="Get all companies"
 )
-async def all_companies():
-    return "All companies"
+async def get_all_companies(db: Session = Depends(get_db)):
+    return CompanyService.get_all(db)
 
 
 @router.get(
@@ -17,5 +32,5 @@ async def all_companies():
     status_code=status.HTTP_200_OK,
     name="Get Company by Id",
 )
-async def get_company(company_id: int):
+async def get_company(company_id: str):
     return "Ok"
