d09403f0eb4eb22dce8f6df5e8fb1de64f7b982f
Nabor Torres <arsentau@gmail.com>
2022-08-26 12:56:07 -0300

    Add Company factory

---
+++ src/factories/companies_factory.py
@@ -0,0 +1,40 @@
+from uuid import uuid4
+
+from faker import Faker
+from sqlalchemy.orm import Session
+
+from models.models import Company
+from repositories.company_repository import CompanyRepository
+
+
+class CompanyFactory:
+    """Generates Companies in the DB"""
+    def __init__(self, db: Session) -> Company:
+        """Set the DB session and the quantity of companies that should be created"""
+        self.db = db
+        self._companies_bulk_reactor()
+
+    def _company_creator(self) -> None:
+        """Creates a single fake company"""
+        fake = Faker()
+        new_company = Company(
+            id=str(uuid4()),
+            company_name=fake.company(),
+            address=fake.street_address(),
+            city=fake.city(),
+            state_province=fake.country_code(),
+            country=fake.current_country(),
+            zip_code=fake.postcode(),
+            time_zone="America/New_York",
+            owner_name=fake.first_name_nonbinary(),
+            owner_last_name=fake.last_name_nonbinary(),
+            email=fake.ascii_email(),
+            phone_number=fake.phone_number(),
+            tax_id=fake.isbn13()
+        )
+        self.company = new_company
+
+    def _companies_bulk_reactor(self) -> None:
+        """Bulk companies creator method"""
+        self._company_creator()
+        CompanyRepository.bulk_create(self.db, self.company)
